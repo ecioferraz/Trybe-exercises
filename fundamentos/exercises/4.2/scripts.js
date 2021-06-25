224cf9c4b9e358c94a8b5058a9e794558fa1c6ef
Écio Ferraz <ecioferraz@hotmail.com>
2021-06-25 13:42:36 -0300

    ajustes

--- fundamentos/exercises/4.2/scripts.js
+++ fundamentos/exercises/4.2/scripts.js
@@ -1,9 +1,11 @@
 /* 1. Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ; */
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-console.log(numbers);
+for (let index = 0; index < numbers.length; index += 1) {
+console.log(numbers[index]);
+}
 
-/* Para o segundo exercício, some todos os valores contidos no array e imprima o resultado; */
+/* 2. Para o segundo exercício, some todos os valores contidos no array e imprima o resultado; */
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 let sum = 0;
