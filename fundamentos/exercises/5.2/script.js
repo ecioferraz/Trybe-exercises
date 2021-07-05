e166fdd0c4cb4866a5a83375e668f2eb843e1b39
Écio Ferraz <ecioferraz@hotmail.com>
2021-07-05 20:24:02 -0300

    exercicios parte 2

--- fundamentos/exercises/5.2/script.js
+++ fundamentos/exercises/5.2/script.js
@@ -1,17 +1,18 @@
+// Parte 1
 // 1. Adicione a tag h1 com o texto Exercício 5.2 - JavaScript DOM como filho da tag body ;
 let h1 = document.createElement('h1');
 h1.innerHTML = 'Exercício 5.2 - JavaScript DOM';
 document.body.appendChild(h1);
 
 // 2. Adicione a tag div com a classe main-content como filho da tag body ;
-let div = document.createElement('div');
-div.className = 'main-content';
-document.body.appendChild(div);
+let mainContent = document.createElement('div');
+mainContent.className = 'main-content';
+document.body.appendChild(mainContent);
 
 // 3. Adicione a tag div com a classe center-content como filho da tag div criada no passo 2;
-div = document.createElement('div');
-div.className = 'center-content';
-document.querySelector('.main-content').appendChild(div);
+centerContent = document.createElement('div');
+centerContent.className = 'center-content';
+document.querySelector('.main-content').appendChild(centerContent);
 
 // 4. Adicione a tag p como filho do div criado no passo 3 e coloque algum texto;
 paragraph = document.createElement('p');
@@ -19,14 +20,14 @@ paragraph.innerHTML = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.
 document.querySelector('.center-content').appendChild(paragraph);
 
 // 5. Adicione a tag div com a classe left-content como filho da tag div criada no passo 2;
-div = document.createElement('div');
-div.className = 'left-content';
-document.querySelector('.main-content').appendChild(div);
+leftContent = document.createElement('div');
+leftContent.className = 'left-content';
+document.querySelector('.main-content').appendChild(leftContent);
 
 // 6. Adicione a tag div com a classe right-content como filho da tag div criada no passo 2;
-div = document.createElement('div');
-div.className = 'right-content';
-document.querySelector('.main-content').appendChild(div);
+rightContent = document.createElement('div');
+rightContent.className = 'right-content';
+document.querySelector('.main-content').appendChild(rightContent);
 
 // 7. Adicione uma imagem com src configurado para o valor https://picsum.photos/200 e classe small-image . Esse elemento deve ser filho do div criado no passo 5;
 image = document.createElement('img');
@@ -55,3 +56,28 @@ for (index = 0; index < 3; index += 1) {
   let h3 = document.createElement('h3');
 document.querySelector('.main-content').appendChild(h3);
 }
+
+// Parte 2
+// 1. Adicione a classe title na tag h1 criada;
+h1.className = 'title';
+
+// 2. Adicione a classe description nas 3 tags h3 criadas;
+let h3 = document.querySelectorAll('h3');
+for (let index = 0; index < h3.length; index += 1) {
+  h3[index].className = 'description';
+}
+
+// 3. Remova o div criado no passo 5 (aquele que possui a classe left-content ). Utilize a função .removeChild() ;
+let divs = document.querySelectorAll('div');
+document.querySelector('.main-content').removeChild(divs[2]);
+
+// 4. Centralize o div criado no passo 6 (aquele que possui a classe right-content ). Dica: para centralizar, basta configurar o margin-right: auto do div ;
+document.querySelector('.right-content').style.marginRight = 'auto';
+
+// 5. Troque a cor de fundo do elemento pai da div criada no passo 3 (aquela que possui a classe center-content ) para a cor verde;
+document.querySelector('.center-content').parentNode.style.backgroundColor = 'green';
+
+// 6. Remova os dois últimos elementos ( nove e dez ) da lista criada no passo 8.
+let lis = document.querySelectorAll('li');
+document.querySelector('ul').removeChild(lis[8]);
+document.querySelector('ul').removeChild(lis[9]);
