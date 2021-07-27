411284cb6ad68747b077fbdf5e15aa80e3253dbc
Écio Ferraz <ecioferraz@hotmail.com>
2021-07-27 13:38:00 -0300

    parte 2 e 3

--- fundamentos/exercises/7.3/parte 2/ex01.js
+++ fundamentos/exercises/7.3/parte 2/ex01.js
@@ -1,6 +1,12 @@
 const assert = require('assert');
 // escreva a função addOne aqui
-
+const addOne = (array) => {
+  const output = [];
+  for (let index = 0; index < array.length; index += 1) {
+    output.push(array[index] + 1);
+  }
+  return output;
+}
 
 const myArray = [31, 57, 12, 5];
 const unchanged = [31, 57, 12, 5];
---
+++ fundamentos/exercises/7.3/parte 2/ex04.js
@@ -0,0 +1,24 @@
+const assert = require('assert');
+// escreva a função findTheNeedle aqui
+const findTheNeedle = (array, word) => {
+  let output = -1;
+  for (let index = 0; index < array.length; index += 1) {
+    array[index] === word ? output = Number(index) : null;
+  }
+  return output;
+}
+
+let words = ['house', 'train', 'slide', 'needle', 'book'];
+let expected = 3;
+let output = findTheNeedle(words, 'needle');
+assert.strictEqual(output, expected);
+
+words = ['plant', 'shelf', 'arrow', 'bird'];
+expected = 0;
+output = findTheNeedle(words, 'plant');
+assert.strictEqual(output, expected);
+
+words = ['plant', 'shelf', 'arrow', 'bird'];
+expected = -1;
+output = findTheNeedle(words, 'plat');
+assert.strictEqual(output, expected);
