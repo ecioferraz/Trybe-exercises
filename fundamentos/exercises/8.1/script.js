f03387838f76d501fc697a7d916097c29466c554
Écio Ferraz <ecioferraz@hotmail.com>
2021-07-29 18:28:10 -0300

    ex 8.1

--- fundamentos/exercises/8.1/script.js
+++ fundamentos/exercises/8.1/script.js
@@ -33,3 +33,46 @@ const winner = (bet, winOrLose) => {
 };
 
 console.log(winner(5, check));
+
+// Ex 03
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const checkAnswers = (rightAnswers, studentAnswers) => {
+  let count = 0;
+  for (let index = 0; index < studentAnswers.length; index += 1) {
+    if (studentAnswers[index] === 'N.A') {
+      count = count;
+    } else if (studentAnswers[index] !== rightAnswers[index]) {
+      count -= 0.5;
+    } if (studentAnswers[index] === rightAnswers[index]) {
+      count += 1;
+    };
+  };
+  return `Nota: ${count}`;
+}
+console.log(checkAnswers(RIGHT_ANSWERS, STUDENT_ANSWERS));
+
+// Na minha lógica inicial iria colocar a função acima como parâmetro final para outra função, mas percebi que essa segunda função só traria a string com o resultado final. Entendi e segui a lógica do gabarito nas funções abaixo, apesar de não achar necessário nesse caso específico.
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const grade = (rightAnswers, studentAnswers, checkAnswers) => {
+  let count = 0;
+  for (let index = 0; index < rightAnswers.length; index += 1) {
+    const countReturn = checkAnswers(rightAnswers[index], studentAnswers[index]);
+    count += countReturn;
+  }
+  return `Nota: ${count}`;
+}
+
+console.log(grade(RIGHT_ANSWERS, STUDENT_ANSWERS, (correct, student) => {
+  if (correct === student) {
+    return 1;
+  } else if (student === 'N.A') {
+    return 0;
+  } else {
+    return -0.5;
+  };
+}));
