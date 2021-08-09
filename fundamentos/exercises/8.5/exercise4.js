e6a478244cd2bf60180da0c7d2c2dc315e2c2b29
Écio Ferraz <ecioferraz@hotmail.com>
2021-08-09 15:23:25 -0300

    ex 04 e 05

--- fundamentos/exercises/8.5/exercise4.js
+++ fundamentos/exercises/8.5/exercise4.js
@@ -29,6 +29,7 @@ const people = [
 ];
 
 // escreva filterPeople abaixo
+const filterPeople = (arrPeople) => arrPeople.filter(({ nationality, bornIn }) => (nationality === 'Australian') && (bornIn > 1900 && bornIn < 2001));
 
 const filteredPeople = filterPeople(people);
 
