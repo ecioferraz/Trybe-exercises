37478112ab140ea3c8a0fff673f7259457556047
Écio Ferraz <ecioferraz@hotmail.com>
2021-08-24 12:11:53 -0300

    refatorando

--- fundamentos/exercises/10.3/exb10d3.test.js
+++ fundamentos/exercises/10.3/exb10d3.test.js
@@ -3,20 +3,11 @@ const { randomNumber } = require('./exb10d3');
 jest.mock('./exb10d3');
 
 describe('01 Tests randomNumber function', () => {
-  it('tests if randomNumber() is being called', () => {
-    randomNumber.mockImplementation();
-
-    randomNumber();
-    expect(randomNumber).toHaveBeenCalled();
-  });
-
-  it('tests if randomNumber() returns the expected value', () => {
+  it('tests if randomNumber() is being called, returns the expected value and how many times randomNumber() is being called', () => {
     randomNumber.mockReturnValue(10);
 
     expect(randomNumber()).toEqual(10);
+    expect(randomNumber).toHaveBeenCalled();
+    expect(randomNumber).toHaveBeenCalledTimes(1);
   });
-
-  it('tests how many times randomNumber() is being called', () => {
-    expect(randomNumber).toHaveBeenCalledTimes(2);
-  })
 })
