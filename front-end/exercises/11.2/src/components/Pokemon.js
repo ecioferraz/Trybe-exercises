4b90b450fc30e7e7ab307ae796b0f1b6d34922b5
Écio Ferraz <ecioferraz@hotmail.com>
2021-09-05 17:11:20 -0300

    ajustes

--- front-end/exercises/11.2/src/components/Pokemon.js
+++ front-end/exercises/11.2/src/components/Pokemon.js
@@ -2,13 +2,21 @@ import React, { Component } from 'react';
 
 class Pokemon extends Component {
   render() {
-    const {pokemon: {name, type, averageWeight, image}} = this.props;
+    const { pokemon: { name, type, averageWeight, image } } = this.props;
     return (
       <div className='pokemon'>
-        <p>{name}</p>
-        <p>{type}</p>
-        <p>Average weight: {averageWeight.value} {averageWeight.measurementUnit}</p>
-        <img src={image} alt={name} />
+        <div>
+          <p>
+            {name}
+          </p>
+          <p>
+            {type}
+          </p>
+          <p>
+            Average weight: {averageWeight.value} {averageWeight.measurementUnit}
+          </p>
+        </div>
+        <img src={image} alt={`${name} sprite`} />
       </div>
     );
   }
