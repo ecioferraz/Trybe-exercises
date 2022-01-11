8527d761ad2b6df08995379f29b3a4b9447e585a
Écio Ferraz <ecioferraz@hotmail.com>
2022-01-11 20:06:18 -0300

    ex 04 and 05

--- back-end/exercises/22.3-mocha-chai-e-sinon-testes-de-back-end-com-nodejs/tests/ex01.test;.js
+++ back-end/exercises/22.3-mocha-chai-e-sinon-testes-de-back-end-com-nodejs/tests/ex01.test;.js
@@ -4,58 +4,49 @@ const { naturalNum } = require('../ex01.js');
 
 describe('Checks the naturalNum function', () => {
   describe('Checks when the number is natural', () => {
-    it('tests if the answer is a string', () => {
-      const answer = naturalNum(10);
+    const ANSWER = naturalNum(10);
 
-      expect(answer).to.be.a('string');
+    it('tests if the answer is a string', () => {
+      expect(ANSWER).to.be.a('string');
     });
 
     it('tests if the answer is equal to "Positivo"', () => {
-      const answer = naturalNum(5);
-
-      expect(answer).to.be.eq('Positivo');
+      expect(ANSWER).to.be.eq('Positivo');
     });
   });
 
   describe('Checks when the number is not natural', () => {
-    it('tests if the answer is a string', () => {
-      const answer = naturalNum(-10);
+    const ANSWER = naturalNum(-10);
 
-      expect(answer).to.be.a('string');
+    it('tests if the answer is a string', () => {
+      expect(ANSWER).to.be.a('string');
     });
 
     it('tests if the answer is equal to "Negativo"', () => {
-      const answer = naturalNum(-5);
-
-      expect(answer).to.be.eq('Negativo');
+      expect(ANSWER).to.be.eq('Negativo');
     });
   })
 
   describe('Checks when the number is neutral', () => {
+    const ANSWER = naturalNum(0);
     it('tests if the answer is a string', () => {
-      const answer = naturalNum(0);
-
-      expect(answer).to.be.a('string');
+      expect(ANSWER).to.be.a('string');
     });
 
     it('tests if the answer is equal to "Neutro"', () => {
-      const answer = naturalNum(0);
-
-      expect(answer).to.be.eq('Neutro');
+      expect(ANSWER).to.be.eq('Neutro');
     });
   })
 
   describe('Checks when the parameter is not a number', () => {
-    it('tests if the answer is a string', () => {
-      const answer = naturalNum('not-a-number');
+    const ANSWER = naturalNum('not-a-number');
 
-      expect(answer).to.be.a('string');
+    it('tests if the answer is a string', () => {
+      expect(ANSWER).to.be.a('string');
     });
 
     it('tests if the answer is "not-a-number não é um número."', () => {
-      const answer = naturalNum('not-a-number');
-
-      expect(answer).to.be.eq('not-a-number não é um número.');
+      expect(ANSWER).to.be.eq('not-a-number não é um número.');
     })
   })
 })
