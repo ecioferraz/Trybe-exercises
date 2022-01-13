833a18b1854ef07bcf0658c19ec39565aaa7d5a4
Écio Ferraz <ecioferraz@hotmail.com>
2022-01-12 21:01:25 -0300

    ex 05

--- back-end/exercises/22.4-http-com-nodejs/index.js
+++ back-end/exercises/22.4-http-com-nodejs/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const rescue = require('express-rescue');
 
+const { getSimpsons, setSimpsons } = require('./fs-utils.js');
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
@@ -23,5 +25,43 @@ app.post('/greetings', (req, res) => {
 
 // ex 01
 app.get('/ping', (_req, res) => res.json({ message: 'pong' }));
+
+// ex04
+app.put('/users/:name/:age', (req, res) => {
+  const { name, age } = req.params;
+  res.status(200).json({ message: `Seu nome é ${name} e você tem ${age} anos de idade` });
+});
+
+//ex 05
+app.get('/simpsons', rescue(async (req, res) =>{
+  const simpsons = await getSimpsons();
+  res.status(200).json(simpsons);
+}));
+
+app.get('/simpsons/:id', rescue(async (req, res) => {
+  const { id } = req.params;
+  const simpsons = await getSimpsons();
+  const simpson = simpsons.find((simp) => simp.id === id);
+
+  if (!simpson) return res.status(404).json({ message: 'Simpson not found!' });
+
+  res.status(202).json(simpson);
+}));
+
+app.post('/simpsons', rescue(async (req, res) => {
+  const { id, name } = req.body;
+  const simpsons = await getSimpsons();
+
+  const isSimpson = simpsons.some((simpson) => simpson.id === id);
+  if (isSimpson) return res.status(409).json({ message: 'Id already exists!' });
+
+  simpsons.push({ id, name });
+  console.log(simpsons);
+  await setSimpsons(simpsons);
+
+  res.status(204).end();
+}));
+
+app.use((err, _req, res, _next) => res.status(500).send(`Something went wrong! Message: ${err.message}`));
 
 app.listen(3001, () => console.log('Aplicação ouvindo na porta 3001'));
