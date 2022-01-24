65dc84349a6c23a1df70bf03c43dd5400aaaf00d
Écio Ferraz <ecioferraz@hotmail.com>
2022-01-24 11:22:10 -0300

    ex 1 and 2

--- back-end/exercises/23.1-arquitetura-de-software-model/middlewares/handleError.js
+++ back-end/exercises/23.1-arquitetura-de-software-model/middlewares/handleError.js
@@ -1,7 +1,7 @@
 const handleError = (err, _req, res, _next) => {
   if (err.status) return res.status(err.status).json({ message: err.message });
   console.error(err);
-  res.status(500).json({ message: 'Server internal error' });
+  res.status(500).json({ message: 'Internal server error' });
 };
 
 module.exports = handleError;
