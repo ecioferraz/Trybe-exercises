e8460e27e22a7a98215709f06ff9f3caa548fe7b
Écio Ferraz <ecioferraz@hotmail.com>
2022-04-07 17:42:16 -0300

    fix

--- back-end/exercises/30.1-mongodb-e-arquitetura-msc/src/schemas/TournamentSchema.ts
+++ back-end/exercises/30.1-mongodb-e-arquitetura-msc/src/schemas/TournamentSchema.ts
@@ -1,7 +1,17 @@
 import { Schema } from 'mongoose';
 import { ITournament } from '../interfaces';
 
-export const TournamentSchema = new Schema<ITournament>()
+export const TournamentSchema = new Schema<ITournament>({
+  year: { type: Number, required: true },
+  hostCountry: { type: String, required: true },
+  champions: { type: String, required: true },
+  runnerUp: { type: String, required: true },
+  editionGoals: { type: Number, required: false },
+  editionStrikers: { type: [{ type: String }], required: true },
+  bestPlayer: { type: String, required: true },
+  bestGoalkeeper: { type: String },
+  bestYoungPlayer: { type: String, required: true },
+})
 
 /* Schema from course's answer
 import { Schema, Document, Types } from 'mongoose';
