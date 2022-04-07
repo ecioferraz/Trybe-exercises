5c5e4eaf7ca8e06c6f27aa98c1fc21e71b2d4f5e
Écio Ferraz <ecioferraz@hotmail.com>
2022-04-07 14:09:53 -0300

    ex 01

---
+++ back-end/exercises/30.1-mongodb-e-arquitetura-msc/src/schemas/TourmentSchema.ts
@@ -0,0 +1,13 @@
+// import { Schema } from 'mongoose';
+
+// export interface ITournment {
+//   year: number,
+//   hostCountry: string,
+//   champions: string,
+//   runnerUp: string,
+//   editionGoals: number,
+//   editionStrikers: string[],
+//   bestPlayer: string,
+//   bestGoalkeeper: string,
+//   bestYoungPlayer: string,
+// }
