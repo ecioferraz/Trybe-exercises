2ef86022efb13ac739938148e4ece2e49ef80412
Écio Ferraz <ecioferraz@hotmail.com>
2022-04-08 19:42:32 -0300

    ex 06

---
+++ back-end/exercises/30.2-mongodb-e-poo/src/Controllers/RecordController.ts
@@ -0,0 +1,3 @@
+import { Request, Response } from 'express';
+import Controller, { IRequestWithBody, ResponseError } from '.';
+import RecordService
---
+++ back-end/exercises/30.2-mongodb-e-poo/src/Controllers/index.ts
@@ -0,0 +1,47 @@
+import { Request, Response } from 'express';
+import Service from '../Services';
+
+export type ResponseError = {
+  error: unknown,
+};
+
+export interface IRequestWithBody<T> extends Request {
+  body: T,
+}
+
+enum ControllerErrors {
+  internal = 'Internal Server Error',
+  notFound = 'Record not found',
+  requiredId = 'Id is required',
+  badRequest = 'Bad request',
+}
+
+export default abstract class Controller<T> {
+  abstract route: string;
+
+  protected errors = ControllerErrors;
+
+  constructor(protected service: Service<T>) { }
+
+  public abstract create(
+    req: IRequestWithBody<T>,
+    res: Response<T | ResponseError>,
+  ): Promise<typeof res>;
+
+  public read = async (
+    _req: Request,
+    res: Response<T[] | ResponseError>,
+  ): Promise<typeof res> => {
+    try {
+      const objs = await this.service.read();
+      return res.json(objs);
+    } catch (error) {
+      return res.status(500).json({ error: this.errors.internal });
+    }
+  };
+
+  abstract readOne(
+    req: Request<{ id: string }>,
+    res: Response<T | ResponseError>,
+  ): Promise<typeof res>;
+}
