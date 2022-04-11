4b10859ab71d3e6a505ba37fbf15ba72fba79b6c
Écio Ferraz <ecioferraz@hotmail.com>
2022-04-11 13:56:23 -0300

    fix

--- back-end/exercises/30.2-mongodb-e-poo/src/Models/Connection.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const connection = (
-  mongoDatabaseURI = process.env.MONGO_URI
-    || 'mongodb://localhost:27017/TrybeStore',
-) => mongoose.connect(mongoDatabaseURI);
-
-export default connection;
