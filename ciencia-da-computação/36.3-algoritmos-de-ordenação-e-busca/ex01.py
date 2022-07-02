93682944d2ed386fc61fd18ed8cfa618464f99bc
Écio Ferraz <ecioferraz@hotmail.com>
2022-07-02 10:33:22 -0300

    feat: ex 01

---
+++ ciencia-da-computação/36.3-algoritmos-de-ordenação-e-busca/ex01.py
@@ -0,0 +1,3 @@
+["zebra", "macaco", "elefante", "arara", "javali"]  # original list
+["macaco", "elefante", "arara", "javali", "zebra"]  # 1st iteration
+["elefante", "arara", "javali", "macaco", "zebra"]  # 2nd iteration
