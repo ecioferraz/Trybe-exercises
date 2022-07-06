ec3bb440cc21c7466b1bbea74d34a342830363ae
Écio Ferraz <ecioferraz@hotmail.com>
2022-07-06 20:32:18 -0300

    feat: ex04

--- ciencia-da-computação/37.1-arquitetura-de-computadores/pc/resources.py
+++ ciencia-da-computação/37.1-arquitetura-de-computadores/pc/resources.py
@@ -1,14 +1,15 @@
 from subprocess import check_output
+import threading
 
 cpu_info = check_output("lscpu").decode("UTF-8").split("\n")
 
 wanted = {
-  "Modelo": "Nome do modelo",
-  "Quantidade de cores": "CPU(s)",
-  "Velocidade (MHz)": "CPU MHz",
-  "Cache L1": "cache de L1i",
-  "Cache L2": "cache de L2",
-  "Cache L3": "cache de L3",
+    "Modelo": "Nome do modelo",
+    "Quantidade de cores": "CPU(s)",
+    "Velocidade (MHz)": "CPU MHz",
+    "Cache L1": "cache de L1i",
+    "Cache L2": "cache de L2",
+    "Cache L3": "cache de L3",
 }
 
 for title, index in wanted.items():
@@ -19,15 +20,20 @@ for title, index in wanted.items():
 
 
 memory_info = [
-  info
-  for info in check_output("free")
-  .decode("UTF-8")
-  .split("\n")[1]
-  .split(" ")
-  if info
-  ]
+    info
+    for info in check_output("free").decode("UTF-8").split("\n")[1].split(" ")
+    if info
+]
 
-print(
-  f"Memória total: {int(memory_info[1]) / 1000} Mb\n"
-  f"Memória disponível: {int(memory_info[2]) / 1000} Mb"
-)
+
+# ref https://stackoverflow.com/questions/3393612/
+# run-certain-code-every-n-seconds
+def print_every_second():
+    threading.Timer(1.0, print_every_second).start()
+    print(
+        f"Memória total: {int(memory_info[1]) / 1000} Mb\n"
+        f"Memória usada: {int(memory_info[2]) / 1000} Mb"
+    )
+
+
+print_every_second()
