c47c327687c9acbc1808f114afd842f80e4d533e
Écio Ferraz <ecioferraz@hotmail.com>
2022-07-06 20:18:50 -0300

    feat: ex03

---
+++ ciencia-da-computação/37.1-arquitetura-de-computadores/pc/resources.py
@@ -0,0 +1,33 @@
+from subprocess import check_output
+
+cpu_info = check_output("lscpu").decode("UTF-8").split("\n")
+
+wanted = {
+  "Modelo": "Nome do modelo",
+  "Quantidade de cores": "CPU(s)",
+  "Velocidade (MHz)": "CPU MHz",
+  "Cache L1": "cache de L1i",
+  "Cache L2": "cache de L2",
+  "Cache L3": "cache de L3",
+}
+
+for title, index in wanted.items():
+    for info in cpu_info:
+        if info.startswith(index):
+            info = info.split("  ")
+            print(f"{title}: {info[-1]}")
+
+
+memory_info = [
+  info
+  for info in check_output("free")
+  .decode("UTF-8")
+  .split("\n")[1]
+  .split(" ")
+  if info
+  ]
+
+print(
+  f"Memória total: {int(memory_info[1]) / 1000} Mb\n"
+  f"Memória disponível: {int(memory_info[2]) / 1000} Mb"
+)
